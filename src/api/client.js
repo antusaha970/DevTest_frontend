6e898fbe9e4fa498492f5b4dd929aa94c9ba317d
Antu Saha <antusaha970@gmail.com>
2024-10-25 12:56:55 +0600

    added live link for backend in client

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://devtest-5y77.onrender.com",
 });
 
 export default client;
